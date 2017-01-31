3022881e87959999e88ffa00676a0884b52f782c
tim seo <gdurtyfive@gmail.com>
2017-01-30 21:04:50 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,55 @@
+console.log('Connection established!')
+
+//////////////////////////////////
+//           PART 1             //
+//////////////////////////////////
+
+// Make a deck of cards.
+
+// 1. Make an array of suits.
+
+// 2. Make an array of values.
+
+// 3. Write a constructor function for Card which takes two arguments, suit and val. Include a stateValue method to return the card's suit and value in a concatenated string.
+
+// 4. Using iteration, build a deck of cards.
+
+// 5. Add an event listener to console.log the deck object and the deck length when a specific button is clicked (don't forget to create the button in your HTML!)
+
+
+//////////////////////////////////
+//           PART 2             //
+//////////////////////////////////
+
+// Make a list of animals in a shelter.
+
+// 1. Make a constructor function for animals that takes name, species, breed, and available as arguments.
+
+// 2. Make three animals and save them each to variables.
+
+// 3. Make an object to represent the shelter. Give it a name, location, and an empty list of animals.
+
+// 4. Add the animals that you made in step 2 to the shelter that you made in step 3.
+
+// 5. Add an event listener to console.log the shelter object when a specific button is clicked (don't forget to create the button in your HTML!)
+
+//////////////////////////////////
+//           PART 3             //
+//////////////////////////////////
+
+// Make a list of animals in a shelter again, but this time, use prototypes.
+
+// 1. Make a constructor function for animals that takes no arguments. Its only property is an identify function which returns a string stating its name and species.
+
+// 2. Make a Cat prototype which inherits from Animal. The constructor function should take three arguments: name, available, and breed.
+
+// 3. Repeat step 2 for Dog.
+
+// 4. Make three animals using the Cat and Dog prototypes and save them each to variables.
+
+// 5. Make an object to represent the shelter. Give it a name, location, and an empty list of animals.
+
+// 6. Add the animals that you made in step 4 to the shelter that you made in step 5.
+
+// 7. Add an event listener to console.log the shelter object when a specific button is clicked (don't forget to create the button in your HTML!)
+
